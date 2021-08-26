a0ffe81c34422d71ec08daea917c27ad6fdc85f2
suneel <suneelpanday@gmail.com>
2021-08-27 01:09:46 +0545

    small changes

--- Live_emotion_detection.py
+++ Live_emotion_detection.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 import PIL
 from tensorflow.keras.models import load_model
-import matplotlib.pyplot as plt
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1' #to use cpu only
 
 saved_model=load_model('fer_model.h5')
 emotions = {0: 'Angry',
@@ -25,25 +27,25 @@ cap=cv2.VideoCapture(0)
 while True:
     ret,img=cap.read()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # plt.imshow(img)
-    # plt.show()
+   
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-    # Detcting face in the image so we can feed it to the  model for emotion prediction
     faces = face_cascade.detectMultiScale(img)
+
     for x, y, w, h in faces:
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (225, 0, 0), 2)
         # feeding only face part of image to the CNN model for accurate prediction
         predicted_emotion = predict_emotion(img[y:y + h, x:x + w])
         cv2.putText(img, predicted_emotion, (x + 20, y + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (225, 0, 0), 2)
         emoji = PIL.Image.open(f'emoji/{predicted_emotion}.png')
-        emoji=emoji.resize((100,100))
-        #x,y=emoji.size
+        emoji=emoji.resize((100,100))  
         img=PIL.Image.fromarray(img)
         img.paste(emoji,(x,y-100,x+100,y))
         img=np.asarray(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     key = cv2.waitKey(1)
     cv2.imshow('Facial Emotion recognition', img)
+
+    #press q to exit
     if key == ord('q'):
         break
 
